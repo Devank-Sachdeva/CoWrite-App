ef3ffc05ff80417e63b08ed11e2ea4fc6810dfcc
Devank Sachdeva <devanksachdeva@gmail.com>
2024-06-03 22:55:57 +0530

    added starting code for text editor enforced layout

--- components/Slate-component.tsx
+++ components/Slate-component.tsx
@@ -1,14 +1,27 @@
 "use client"
 
-import { useState } from 'react'
-import { createEditor } from 'slate'
-
-import { Slate, Editable, withReact } from 'slate-react'
-
-import { BaseEditor, Descendant } from 'slate'
+import { useCallback, useMemo, useState } from 'react'
+import { Slate, Editable, withReact, RenderElementProps } from 'slate-react'
+import {
+    BaseEditor,
+    Transforms,
+    createEditor,
+    Node,
+    Element as SlateElement,
+    Descendant,
+    Editor,
+} from 'slate'
 import { ReactEditor } from 'slate-react'
 
-type CustomElement = { type: 'paragraph'; children: CustomText[] }
+
+type CustomElement = TitleElement | ParagraphElement
+type ParagraphElement = {
+    type: 'paragraph'
+    align?: string
+    children: Descendant[]
+}
+
+type TitleElement = { type: 'title'; children: Descendant[] }
 type CustomText = { text: string }
 
 declare module 'slate' {
@@ -19,13 +32,102 @@ declare module 'slate' {
     }
 }
 
-const initialValue : CustomElement[] = [{type: 'paragraph', children: [{ text: 'A line of text in a paragraph.' }]}]
+type ElementType = "title" | "paragraph" | undefined
+
+const initialValue: Descendant[] = [
+    {
+        type: 'title',
+        children: [{ text: 'Enforce Your Layout!' }],
+    },
+    {
+        type: 'paragraph',
+        children: [
+            {
+                text: 'This example shows how to enforce your layout with domain-specific constraints. This document will always have a title block at the top and at least one paragraph in the body. Try deleting them and see what happens!',
+            },
+        ],
+    },
+]
 
 export const  Component = () => {
-    const [editor] = useState(() => withReact(createEditor()));
+    const renderElement = useCallback((props: RenderElementProps) => <Element {...props} />, [])
 
+    const editor = useMemo(
+        () => withLayout(withReact(createEditor())),
+        []
+    )
     return <Slate editor={editor} initialValue={initialValue} > 
-        <Editable /> 
+        <Editable
+            renderElement={renderElement}
+            placeholder="Enter a title…"
+            spellCheck
+            autoFocus
+        />
     </Slate>
 
+}
+
+const withLayout = (editor : Editor) => {
+    const { normalizeNode } = editor
+    editor.normalizeNode = ([node, path]) => {
+        if (path.length === 0) {
+            if (editor.children.length <= 1 && Editor.string(editor, [0, 0]) === '') {
+                const title: TitleElement = {
+                    type: 'title',
+                    children: [{ text: 'Untitled' }],
+                }
+                Transforms.insertNodes(editor, title, {
+                    at: path.concat(0),
+                    select: true,
+                })
+            }
+
+            if (editor.children.length < 2) {
+                const paragraph: ParagraphElement = {
+                    type: 'paragraph',
+                    children: [{ text: '' }],
+                }
+                Transforms.insertNodes(editor, paragraph, { at: path.concat(1) })
+            }
+            
+            for (const [child, childPath] of Node.children(editor, path)) {
+                let type: ElementType
+                const slateIndex = childPath[0]
+                const enforceType = ( type : ElementType ) => {
+                    if (SlateElement.isElement(child) && child.type !== type) {
+                        const newProperties: Partial<SlateElement> = { type }
+                        Transforms.setNodes<SlateElement>(editor, newProperties, {
+                            at: childPath,
+                        })
+                    }
+                }
+
+                switch (slateIndex) {
+                    case 0:
+                        type = 'title'
+                        enforceType(type as ElementType)
+                        break
+                    case 1:
+                        type = 'paragraph'
+                        enforceType(type as ElementType)
+                    default:
+                        break
+                }
+            }
+        }
+
+        return normalizeNode([node, path])
+    }
+
+    return editor
+}
+
+
+const Element = ({ attributes, children, element } : RenderElementProps) => {
+    switch (element.type) {
+        case 'title':
+            return <div className='text-3xl focus:border-none' {...attributes}>{children}</div>
+        case 'paragraph':
+            return <p {...attributes}>{children}</p>
+    }
 }
---
+++ components/TextEditor.tsx
@@ -0,0 +1,11 @@
+// import { BaseEditor } from "slate";
+// import { ReactEditor, RenderElementProps } from "slate-react";
+
+// export const Element = ({ attributes, children, element}: RenderElementProps) => {
+//     switch(element.type){
+//         case "paragraph": 
+//             return <p {...attributes}>{children}</p>
+//         case "code"
+//     }
+//     return <div>{children}</div>
+// }
